67590fb24a1be2ba296039c4bca2f95238cda363
Nick Klauer <klauer@gmail.com>
2012-02-19 18:37:08 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject stacktraces "1.0.0-SNAPSHOT"
+  :description "http REST client for the v2.0 StackExchange API (http://api.stackoverflow.com)"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.2"]
+                 [cheshire "2.2.0"]]
+  :dev-dependencies [[marginalia "0.7.0-SNAPSHOT"]
+                     [midje "1.3.2-SNAPSHOT"]])
