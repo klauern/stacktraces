444265b8bb99ebf1c7cfa284c8bcf6c91edb6ae1
Nick Klauer <klauer@gmail.com>
2012-02-23 06:00:35 -0600

    add to the wrapper http get request to handle query params

--- src/stacktraces/core.clj
+++ src/stacktraces/core.clj
@@ -9,18 +9,23 @@
 
 (def url "http://api.stackexchange.com/2.0")
 
-(defn get-errors 
+(defn get-errors
   ([] (http/get (str url "/errors"))) 
   ([err-code] (http/get (str url "/errors/" err-code) default-http-parms)))
 
-(defn get-sxch-sites 
-  ([] (http/get (str url "/sites")))
-  ([opts] (http/get ()))
-  ([page per-page] (http/get (str url "/sites/" ))))
+(defn get-sxch-sites
+  "Get a list of the StackExchange sites"
+  ([] (make-get-request "/sites"))
+  ([opts] (make-get-request "/sites" opts))
+  ([page per-page] (http/get (str url "/sites") {:page page :pagesize per-page})))
 
 (defn site-info [site-name]
+  "Get the Site info"
   (make-get-request "info" {:site site-name}))
 
 
 (defn make-get-request [api-method params]
-  (http/get (str url api-method "?" (helper/map-params params))))
+  "make an HTTP GET request agains the API, passing in any optional query params"
+  ([api-method] (http/get (str url api-method)))
+  ([api-method params]
+     (http/get (str url api-method "?") {:query-params params})))
