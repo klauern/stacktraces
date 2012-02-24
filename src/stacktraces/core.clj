821f3d899eb21a54ad2a903725031e530f315fb7
Nick Klauer <klauer@gmail.com>
2012-02-23 21:30:07 -0600

    reorg things, wrap calls to api

--- src/stacktraces/core.clj
+++ src/stacktraces/core.clj
@@ -5,27 +5,29 @@
 
 ;; I'm not letting it throw exceptions, which I know I should, but I
 ;; don't know how to wrap it properly (yet).  This is a TODO
-(def default-http-parms {:throw-exceptions false :ignore-unknown-host? true :as :json})
+(def default-http-params {:throw-exceptions false :ignore-unknown-host? true :as :json})
 
 (def url "http://api.stackexchange.com/2.0")
 
+(defn make-get-request
+  "make an HTTP GET request agains the API, passing in any optional query params"
+  ([api-method] (http/get (str url api-method) default-http-params))
+  ([api-method params]
+     (http/get (str url api-method "?") (merge default-http-params {:query-params params}))))
+
 (defn get-errors
-  ([] (http/get (str url "/errors"))) 
-  ([err-code] (http/get (str url "/errors/" err-code) default-http-parms)))
+  "List all of the error types in the StackExchange world.  If passed an err-code, look it up and return the right value for the error explaining what it means."
+  ([] (make-get-request "/errors")) 
+  ([err-code] (make-get-request (str url "/errors/" err-code))))
 
 (defn get-sxch-sites
-  "Get a list of the StackExchange sites"
+  "Get a list of the StackExchange sites."
   ([] (make-get-request "/sites"))
   ([opts] (make-get-request "/sites" opts))
-  ([page per-page] (http/get (str url "/sites") {:page page :pagesize per-page})))
+  ([page per-page] (make-get-request "/sites" {:page page :pagesize per-page})))
 
 (defn site-info [site-name]
   "Get the Site info"
   (make-get-request "info" {:site site-name}))
 
 
-(defn make-get-request [api-method params]
-  "make an HTTP GET request agains the API, passing in any optional query params"
-  ([api-method] (http/get (str url api-method)))
-  ([api-method params]
-     (http/get (str url api-method "?") {:query-params params})))
